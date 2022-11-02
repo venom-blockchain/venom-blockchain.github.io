52a8cfd2200e4efec4676762603bb99a68b7fa4f
R Kalt <roman@venom.mail>
2022-11-02 18:11:56 +0400

    fix: some redirects

---
+++ redirects.js
@@ -0,0 +1,32 @@
+module.exports = {
+  redirects: [
+    {
+      to: '/build/development-guides/how-to-create-your-own-fungible-tip-3-token/fungible-tokens-in-venom-network',
+      from: '/build/development-guides/how-to-create-your-own-fungible-tip-3-token'
+    },
+    {
+      to: '/build/development-guides/developing-of-simple-voting-system/voting-system-basics',
+      from: '/build/development-guides/developing-of-simple-voting-system'
+    },
+    {
+      to: '/build/development-guides/how-to-create-your-own-non-fungible-tip-4-token/non-fungible-tokens-in-venom-network',
+      from: '/build/development-guides/how-to-create-your-own-non-fungible-tip-4-token'
+    },
+    {
+      to: '/build/integration-guides/how-to-connect-dapp-ui-to-venom',
+      from: '/build/integration-guides'
+    },
+    {
+      to: '/general/readme',
+      from: '/general'
+    },
+    {
+      to: '/learn/glossary',
+      from: '/learn'
+    },
+    {
+      to: '/maintain/network-maintainers',
+      from: '/maintain'
+    },
+  ]
+}
