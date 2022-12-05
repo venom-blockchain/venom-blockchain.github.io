89f52da52202e6c49eb1d24600fa20ca52cc1f11
R <roman@venom.email>
2022-12-05 14:00:27 +0400

    feat: npm gtm plugin changed to manual injecting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,41 @@ const config = {
       },
     ],
     [
-      require.resolve('docusaurus-gtm-plugin'),
+      (_, options) => {
+        return {
+          name: 'my-gtm-docusaurus-plugin',
+          injectHtmlTags() {
+            if (!(process.env.NODE_ENV === 'production')) {
+              return {}
+            }
+            return {
+              headTags: [
+                {
+                  tagName: 'link',
+                  attributes: {
+                    rel: 'preconnect',
+                    href: 'https://www.googletagmanager.com',
+                  },
+                },
+                {
+                  tagName: 'script',
+                  innerHTML: `
+                    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${options.id}');</script>
+                  `,
+                },
+              ],
+              preBodyTags: [
+                `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${options.id}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+              ],
+            }
+          },
+        }
+      },
       {
         id: 'GTM-MQRC9JG', // GTM Container ID
       }
